ea8b5303794fd04de190bfc2c7b173c153fd6e4f
Ryan Attard <ryanattard@gmail.com>
2023-11-23 13:32:00 +0100

    entity data model

---
+++ OOP2023_ClassExamples/DataAccess/MyConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace DataAccess
+{
+    public class MyConnection
+    {
+        public ShoppingCartOOPEntities Context { get; set; } 
+        //property representing the auto-generated class which allows me to access the database
+
+        public MyConnection() {
+            Context = new ShoppingCartOOPEntities();
+        }
+    }
+}
---
+++ OOP2023_ClassExamples/DataAccess/ProductsRepository.cs
@@ -0,0 +1,63 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ProductsRepository: MyConnection
+    {
+        public ProductsRepository():base() { }
+
+
+
+        //List -> it makes a call to the database immediately
+        //IQueryable -> it postpones the call to the database until you decide
+
+        /// <summary>
+        /// This method will return all the products from the database
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<Product> GetProducts() { 
+            //Select * From Products
+
+            return Context.Products.AsQueryable(); //gets the entire list of products
+
+
+
+        }
+
+        public Product GetProductById(int id) { 
+
+            //Select Top 1 Id, Name, Price From Products Where Id == $id
+
+            return GetProducts().SingleOrDefault(p => p.Id == id);
+        
+        }
+
+        public IQueryable<Product> GetProducts(string keyword) { 
+            //Select * From Products Where Name like '%$keyword%'
+
+        return GetProducts().Where(p => p.Name.Contains(keyword)).OrderByDescending(x=>x.Price);
+        
+        }
+
+        public void AddProduct(Product product) { 
+        
+            Context.Products.Add(product);//adds the product in memory to the context (property) which is representing the database
+            Context.SaveChanges(); //commits/saves what's inside the memory into the database
+
+        }
+
+        public void DeleteProduct(Product product) { }  
+
+        public void UpdateProduct(Product product) { }
+
+      
+
+    
+
+    }
+}
